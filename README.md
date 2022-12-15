478efd856378fc573d9757d7ce60cee4f8ee58b9
Bhanu Vanam <bhanu@Bhanus-Air.cable.rcn.com>
2022-12-15 18:09:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+
+                        Object Detection on DeepFashion dataset using DETR 
+                             Advanced Computer Vision (EECE 7370)
+
+           
+Team:
+Bhanu Sai Simha Vanam
+Avinash Deyyam
+Naga Sindhu Namana
+
+Follow the prerequisite steps and requirements
+
+
+conda create -n detr_detection python=3.7
+conda activate detr_detection
+pip install -r requirements.txt
+
+-------------------------------- Training -----------------------------
+
+1. Download DeepFashion Dataset from the official source https://drive.google.com/drive/folders/125F48fsMBz2EF0Cpqk6aaHet5VH399Ok. unzip and place it in your desired path.
+
+2. Adjust the dataset_path in the train.ipynb notebook and run all cells.
+3. After training with the default training parameters for roughly 2 days, the model with be saved in detr/outputs as checkpoint.pth (I later renamed it to fashionobjectdetection.pth for readability and inference purpose)
+
+-------------------------------- Inference -----------------------------
+
+1. Download our trained model from {} and place it in your desired location
+2. Adjust the model_path and test image path in the infertence.ipynb notebook and run all the cells
+
+
---
+++ train_hugging.py
@@ -0,0 +1,207 @@
+import torchvision
+import os
+
+class CocoDetection(torchvision.datasets.CocoDetection):
+    def __init__(self, img_folder, feature_extractor, train=True):
+        ann_file = os.path.join(img_folder, "custom_train.json" if train else "custom_val.json")
+        super(CocoDetection, self).__init__(img_folder, ann_file)
+        self.feature_extractor = feature_extractor
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        img, target = super(CocoDetection, self).__getitem__(idx)
+        
+        # preprocess image and target (converting target to DETR format, resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {'image_id': image_id, 'annotations': target}
+        encoding = self.feature_extractor(images=img, annotations=target, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze() # remove batch dimension
+        target = encoding["labels"][0] # remove batch dimension
+
+        return pixel_values, target
+
+from transformers import DetrFeatureExtractor
+
+feature_extractor = DetrFeatureExtractor.from_pretrained("facebook/detr-resnet-50")
+
+train_dataset = CocoDetection(img_folder='/scratch/vanam.b/dataset/train2017', feature_extractor=feature_extractor)
+val_dataset = CocoDetection(img_folder='/scratch/vanam.b/dataset/val2017', feature_extractor=feature_extractor, train=False)
+
+
+print("Number of training examples:", len(train_dataset))
+print("Number of validation examples:", len(val_dataset))
+
+
+
+import numpy as np
+import os
+from PIL import Image, ImageDraw
+
+# based on https://github.com/woctezuma/finetune-detr/blob/master/finetune_detr.ipynb
+image_ids = train_dataset.coco.getImgIds()
+# let's pick a random image
+image_id = image_ids[np.random.randint(0, len(image_ids))]
+print('Image n°{}'.format(image_id))
+image = train_dataset.coco.loadImgs(image_id)[0]
+image = Image.open(os.path.join('/scratch/vanam.b/dataset/train2017', image['file_name']))
+
+annotations = train_dataset.coco.imgToAnns[image_id]
+draw = ImageDraw.Draw(image, "RGBA")
+
+cats = train_dataset.coco.cats
+id2label = {k: v['name'] for k,v in cats.items()}
+
+for annotation in annotations:
+  box = annotation['bbox']
+  class_idx = annotation['category_id']
+  x,y,w,h = tuple(box)
+  draw.rectangle((x,y,x+w,y+h), outline='red', width=1)
+  draw.text((x, y), id2label[class_idx], fill='white')
+
+image
+
+
+
+from torch.utils.data import DataLoader
+
+def collate_fn(batch):
+  pixel_values = [item[0] for item in batch]
+  encoding = feature_extractor.pad_and_create_pixel_mask(pixel_values, return_tensors="pt")
+  labels = [item[1] for item in batch]
+  batch = {}
+  batch['pixel_values'] = encoding['pixel_values']
+  batch['pixel_mask'] = encoding['pixel_mask']
+  batch['labels'] = labels
+  return batch
+
+train_dataloader = DataLoader(train_dataset, collate_fn=collate_fn, batch_size=4, shuffle=True)
+val_dataloader = DataLoader(val_dataset, collate_fn=collate_fn, batch_size=2)
+batch = next(iter(train_dataloader))
+
+batch.keys()
+
+pixel_values, target = train_dataset[0]
+
+pixel_values.shape
+
+print(target)
+
+
+import pytorch_lightning as pl
+from transformers import DetrConfig, DetrForObjectDetection
+import torch
+
+class Detr(pl.LightningModule):
+
+     def __init__(self, lr, lr_backbone, weight_decay):
+         super().__init__()
+         # replace COCO classification head with custom head
+         self.model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", 
+                                                             num_labels=len(id2label),
+                                                             ignore_mismatched_sizes=True)
+         # see https://github.com/PyTorchLightning/pytorch-lightning/pull/1896
+         self.lr = lr
+         self.lr_backbone = lr_backbone
+         self.weight_decay = weight_decay
+
+     def forward(self, pixel_values, pixel_mask):
+       outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+       return outputs
+     
+     def common_step(self, batch, batch_idx):
+       pixel_values = batch["pixel_values"]
+       pixel_mask = batch["pixel_mask"]
+       labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
+
+       outputs = self.model(pixel_values=pixel_values, pixel_mask=pixel_mask, labels=labels)
+
+       loss = outputs.loss
+       loss_dict = outputs.loss_dict
+
+       return loss, loss_dict
+
+     def training_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)     
+        # logs metrics for each training_step,
+        # and the average across the epoch
+        self.log("training_loss", loss)
+        for k,v in loss_dict.items():
+          self.log("train_" + k, v.item())
+
+        return loss
+
+     def validation_step(self, batch, batch_idx):
+        loss, loss_dict = self.common_step(batch, batch_idx)     
+        self.log("validation_loss", loss)
+        for k,v in loss_dict.items():
+          self.log("validation_" + k, v.item())
+
+        return loss
+
+     def configure_optimizers(self):
+        param_dicts = [
+              {"params": [p for n, p in self.named_parameters() if "backbone" not in n and p.requires_grad]},
+              {
+                  "params": [p for n, p in self.named_parameters() if "backbone" in n and p.requires_grad],
+                  "lr": self.lr_backbone,
+              },
+        ]
+        optimizer = torch.optim.AdamW(param_dicts, lr=self.lr,
+                                  weight_decay=self.weight_decay)
+        
+        return optimizer
+
+     def train_dataloader(self):
+        return train_dataloader
+
+     def val_dataloader(self):
+        return val_dataloader
+
+
+
+model = Detr(lr=1e-4, lr_backbone=1e-5, weight_decay=1e-4)
+
+outputs = model(pixel_values=batch['pixel_values'], pixel_mask=batch['pixel_mask'])
+
+outputs.logits.shape
+
+from pytorch_lightning import Trainer
+
+trainer = Trainer(gpus=1, max_steps=300, gradient_clip_val=0.1)
+trainer.fit(model)
+
+from detr.datasets import get_coco_api_from_dataset
+
+base_ds = get_coco_api_from_dataset(val_dataset) # this is actually just calling the coco attribute
+
+from datasets.coco_eval import CocoEvaluator
+from tqdm.notebook import tqdm
+
+iou_types = ['bbox']
+coco_evaluator = CocoEvaluator(base_ds, iou_types) # initialize evaluator with ground truths
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model.to(device)
+model.eval()
+
+print("Running evaluation...")
+
+for idx, batch in enumerate(tqdm(val_dataloader)):
+    # get the inputs
+    pixel_values = batch["pixel_values"].to(device)
+    pixel_mask = batch["pixel_mask"].to(device)
+    labels = [{k: v.to(device) for k, v in t.items()} for t in batch["labels"]] # these are in DETR format, resized + normalized
+
+    # forward pass
+    outputs = model.model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+    orig_target_sizes = torch.stack([target["orig_size"] for target in labels], dim=0)
+    results = feature_extractor.post_process(outputs, orig_target_sizes) # convert outputs of model to COCO api
+    res = {target['image_id'].item(): output for target, output in zip(labels, results)}
+    coco_evaluator.update(res)
+
+coco_evaluator.synchronize_between_processes()
+coco_evaluator.accumulate()
+coco_evaluator.summarize()
